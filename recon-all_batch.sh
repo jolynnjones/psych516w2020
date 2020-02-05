d7044c471525705ceef1dec0a375d0ee8fcc7c5a
jolynnjones <59666475+jolynnjones@users.noreply.github.com>
2020-02-05 10:59:40 -0700

    step4_recon-all

---
+++ recon-all_batch.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curTime=`date +"%Y%m%d-%H%M%S"`
+mkdir -p ~/logfiles/EDSD/${curTime}-t1
+
+for i in sub-FREAD001; do
+#for i in `cat ~/psych516/EDSD/participants.txt`; do
+	sbatch \
+	-o ~/logfiles/EDSD/${curTime}-t1/o-${i}.txt \
+	-e ~/logfiles/EDSD/${curTime}-t1/e-${i}.txt \
+	~/psych516/scripts/recon-all_job.sh \
+	${i}
+	sleep 1
+done
