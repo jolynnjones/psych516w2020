2cb04c42c6f7a5445fe7bb0050bee756fd70ae8c
jolynnjones <59666475+jolynnjones@users.noreply.github.com>
2020-02-05 11:00:04 -0700

    step4_recon-all_bigventricles

---
+++ recon-all_bigv_job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --time=24:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=16G  # memory per CPU core
+
+# LOAD ENVIRONMENTAL VARIABLES
+freesurfer
+
+# INSERT CODE, AND RUN YOUR PROGRAMS HERE
+cd ~/psych516/EDSD/derivative/${1}/
+
+# RECON-ALL BIG VENTRICLES
+recon-all \
+-subjid ${1} \
+-i ${1}_T1w.nii.gz \
+-wsatlas \
+-all \
+-bigventricles \
+-sd ~/psych516/EDSD/my_dataset/FS_bigv_subjdir/${1}/
