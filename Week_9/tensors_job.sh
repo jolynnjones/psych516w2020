d35692825760469e30ad8593da690c61d48449ed
jolynnjones <59666475+jolynnjones@users.noreply.github.com>
2020-03-28 16:26:46 -0600

    Update tensors_job.sh

--- Week_9/tensors_job.sh
+++ Week_9/tensors_job.sh
@@ -10,8 +10,8 @@ cd ~/psych516/EDSD/pDWI/${1}/dwi/
 #GENERATE EPI MASK
 workdir=~/psych516/EDSD/pDWI/${1}/dwi
 fslroi ${workdir}/${1}_dwi_epi.nii.gz ${workdir}/${1}_dwi_epi_b0.nii.gz 0 1
-bet ${1}_dwi_epi_b0.nii.gz dwi_epi_b0 -f 0.5 -g 0 -m
-bet dwi_epi_b0.nii.gz ${1}_dwi_epi_b0 -f 0.5 -g 0 -m
+bet ${1}_dwi_epi_b0.nii.gz dwi_epi_b0 -f 0.25 -g 0 -m
+bet dwi_epi_b0.nii.gz ${1}_dwi_epi_b0 -f 0.25 -g 0 -m
 
 CALCULATING TENSORS
 dtifit \
